013f098ddcb497450a0f021ef5f94d3c2d3cd87c
jdgiannisii-lang <jdgiannisii@gmail.com>
2026-05-05 09:42:50 -0500

    Rename error variable in stock data fetch

--- api/stock.js
+++ api/stock.js
@@ -7,8 +7,9 @@ export default async function handler(req, res) {
     );
 
     const data = await response.json();
+
     res.status(200).json(data);
-  } catch (error) {
+  } catch (err) {
     res.status(500).json({ error: "Failed to fetch stock data" });
   }
 }
